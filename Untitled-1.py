da37d3c1cc58a36cebd8f9386ee46ef381ad9e91
ki1monw <dubianmycola@gmail.com>
2025-08-26 20:03:57 +0300

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,42 @@
+#print( 200//21 )
+#print( 200//11 )
+#print( 200//221 )
+#float
+##input1 = float( input("Enter a number: ") )
+
+
+
+Name = input("Enter your name: ")
+print(f"Hello {Name}")
+
+a = input("Enter a email: ")
+if "@" not in a:
+    print("one more time: Atempts 3")
+    a = input("Enter a email: ")
+    if "@" not in a:
+        print("one more time: Atempts 2")
+        a = input("Enter a email: ")
+    if "." not in a:
+        print("one more time: Atempts 1")
+        a = input("Enter a email: ")
+        if "@" not in a:
+            print("Atempts 0")
+            exit()
+
+
+#print(f"Email: {a}")
+
+b = input("Enter nomber ur phone: ")
+
+#print(f"Phone number: {b}")
+
+c = input("Enter a masawge: ")
+
+#print(f"Message 1: {c}")
+
+d = input("Enter a masawge: ")
+
+#print(f"Message 2: {d}")
+
+print(a,b,c,d)
+
